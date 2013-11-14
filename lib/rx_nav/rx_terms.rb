a42ad2c0adcb68edb91197e265850f39ba34b6fe
Jonathan Bender <jlbender@gmail.com>
2013-11-14 18:54:07 -0500

    add rx terms methods

--- lib/rx_nav/rx_terms.rb
+++ lib/rx_nav/rx_terms.rb
@@ -1,5 +1,24 @@
 module RxNav
   class RxTerms
+    class << self
+      def all_concepts
+        query = "/allconcepts"
+        data = get_response_hash(query)[:min_concept_group]
+        return data.map{ |c| OpenStruct.new(c) }
+      end
 
+      def all_info id
+        query = "/rxcui/#{id}/allinfo"
+        data = get_response_hash(query)[:rxterms_properties]
+        return OpenStruct.new(data)
+      end
+
+      private
+
+      def get_response_hash query
+        request = URI.parse(RxNav::BASE_URL + '/RxTerms' + query)
+        return RxNav.nori.parse(Net::HTTP.get request)[:rxtermsdata]
+      end
+    end
   end
 end
